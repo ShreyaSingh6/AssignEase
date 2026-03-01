e0f3883ef9de78e5a5fada8eec345db0d4cb39bf
SHREYA SINGH <167967051+ShreyaSingh6@users.noreply.github.com>
2026-03-01 22:03:26 +0530

    Update settings.py

--- Assignment_Submission/settings.py
+++ Assignment_Submission/settings.py
@@ -50,7 +50,7 @@ import os
 import dj_database_url
 
 DEBUG = True
-ALLOWED_HOSTS = ['web-production-18c57.up.railway.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['web-production-0b470.up.railway.app', '127.0.0.1', 'localhost']
 
 SECRET_KEY = os.environ.get("SECRET_KEY")  # Remove the hardcoded one
 
